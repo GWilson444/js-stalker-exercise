1aec945934949ae3e2794a9c3b490b9a20bb347f
GWilson444 <gregorywilsonlmt@gmail.com>
2020-10-13 23:09:46 -0700

    changed template literals to concat'd strings

--- stalkerScript.js
+++ stalkerScript.js
@@ -2,5 +2,5 @@ let firstName = prompt("What is your first name?");
 let lastName = prompt("What is your last name?");
 let age = prompt("What is your age?");
 
-console.log(`Your full name is ${firstName} ${lastName}.`);
-console.log(`Your are ${age} years old.`)
+console.log("Your full name is " + firstName + " " + lastName + ".");
+console.log("Your are " + age + " " + "years old.")
